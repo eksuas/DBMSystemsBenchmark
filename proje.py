bb479ce253512fcc06cd10fd29c7c73a2e95800f
IremmB <37192705+IremmB@users.noreply.github.com>
2018-07-08 19:38:00 +0300

    Add files via upload

--- proje.py
+++ proje.py
@@ -4,107 +4,112 @@ from py2neo import authenticate, Graph,Node
 import argparse
 from sets import Set
 
-def Arg_Parser():
+# Command line argument parser
+def arg_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--user',   ## Add user argument for server
-    type=str,
-    default="neo4j",
-    help='username for neo4j')
-    parser.add_argument('--password',   #Add password argument for server
-    type=str,
-    default="777",
-    help='password of the user')
-    parser.add_argument('--path',       #Add file path argument for FILMS.txt file
-    default="data/FILMS.txt",
-    help='the path of FILMS.txt file')
+    parser.add_argument('--user',
+        type=str,
+        default="neo4j",
+        help='username for neo4j')
+    parser.add_argument('--password',
+        type=str,
+        default="777",
+        help='password of the user')
+    parser.add_argument('--hostname',
+        default="localhost:7474",
+        help='hostname of the server')
+    parser.add_argument('--path',
+        default="data/FILMS.txt",
+        help='the path of FILMS.txt file')
     try:
         args = parser.parse_args()
 
     except SystemExit as e:
         print(e)
         sys.exit()
-    return args      # Return user,password,path arguments to use in other functions
+    # Return user,password,path arguments to use in other functions
+    return args
 
-def file_operations(args):
+def file_operations(path):
     try:
-        f=open(args.path,"r") # Open file with args.path
+        # Open file with args.path
+        f=open(path,"r")
     except IOError:
-        print("The file is not found.")  #if there is a error, do it
+         #if there is a error, do it
+        print("The file is not found.")
         sys.exit()
+    # Read file
+    text_lines=f.readlines()
+    return(text_lines)
 
-    text_lines=f.readlines()  # Read file
+ #Neo4j operations :
+def neo4j(user,password,hostname,text_lines):
+    try:
+        # Authenticate for server and connect it
+        authenticate (hostname, user, password)
+        graph=Graph()
+    # If server is not connected :
+    except Exception as e:
+        print ("Unable to reach server.")
+        sys.exit()
+    start=graph.begin()
     actor_set=Set()
     director_set=Set()
     i=0
-    while i<len(text_lines):        # to read every line
-
+    while i<len(text_lines):
         line=text_lines[i].split('%')
         i+=1
-        Movie_dict={              # Create dictionary for movies
-        'film_id':line[0],
-        'film_title':line[1],
-        'film_year':line[2],
-        'actor_list':line[3].split(','),  # Divide actors(line[3]=actors) incording to "," and add into actor_list
-        'film_genre':line[4],
-        'director':line[5],
-        'film_rating':line[6],
+        # Create dictionary for movies
+        movies={
+            'id':line[0],
+            'title':line[1],
+            'year':line[2],
+            # Divide actors(line[3]=actors) incording to "," and add into actor_list
+            'actor_list':line[3].split(','),
+            'genre':line[4],
+            'director':line[5],
+            'rating':line[6],
         }
-        Neo4j_movie(Movie_dict,args)  #Send every movie_dict into Neo4j_movie() function to create node
-        director_set.add(line[5])     #Add every new director into director_set
-        for actor in Movie_dict['actor_list']:    #Add every new actor in actor_list into actor_set
+      # Create node for Movies by taking from movies and add properties into node
+        node1=Node("Movies",
+                id=movies['id'],
+                title=movies['title'],
+                released_year=['year'],
+                rating=['rating'],
+                genre=['genre'])
+        #Create it
+        start.merge(node1)
+        #Add every new director into director_set
+        director_set.add(line[5])
+        #Add every new actor in actor_list into actor_set
+        for actor in movies['actor_list']:
             actor_set.add(actor)
 
-    return Movie_dict,director_set,actor_set
-
-def Neo4j_movie(Movie_dict,args): # Neo4j operation of Movies
-
-    try:
-        authenticate ("localhost:7474", args.user, args.password)   # Authenticate for server and connect
-        graph=Graph()
-    except Exception as e:    # If server is not connected
-        print ("Unable to reach server.")
-        sys.exit()
-    start=graph.begin()
-    node1=Node("Movies",            # Create node for Movies by taking from Movie_dict
-        id=Movie_dict['film_id'],        # And add properties into node
-        title=Movie_dict['film_title'],
-        released_year=['film_year'],
-        rating=['film_rating'],
-        genre=['film_genre'])
-    start.merge(node1)   #Create it
-    start.commit()
-
-
-def Neo4j(director_set,actor_set,args): #Neo4j operation of director and actors
-
-    try:
-        authenticate ("localhost:7474", args.user, args.password)   # Authenticate for server and connect
-        graph=Graph()
-    except Exception as e:    # If server is not connected
-        print ("Unable to reach server.")
-        sys.exit()
     director_id=3000
     actor_id=2000
-    start=graph.begin()
     for director in director_set:
-        node2 = Node("Directors",userid=director_id, fullname=director)  # Node for directors in director_set
+        # Node for directors in director_set
+        node2 = Node("Directors",userid=director_id, fullname=director)
         start.merge(node2)   #Create it
         director_id+=1
 
     for actor in actor_set:
-        node3=Node("Actors", userid=actor_id, fullname=actor)    #Node for actors in actor_set
+        #Node for actors in actor_set
+        node3=Node("Actors", userid=actor_id, fullname=actor)
         start.merge(node3)  #Create it
         actor_id+=1
+     # Create all nodes in server
+    start.commit()
 
-    start.commit()   # Create all nodes in server
 
 
-
-def main():#main function
-    args=Arg_Parser() # first function
-    arguments=file_operations(args)  # Call second function and put results(Movie_dict,
-#director_set,actor_set) into arguments
-    Neo4j(arguments[1],arguments[2],args) #Third function for  neo4j operations of directors and actors
-
+#main function
+def main():
+    # first function
+    args=arg_parser()
+    # Call second function and put results(movies, director_set,actor_set) into arguments
+    text_lines=file_operations(args.path)
+    #Third function for  neo4j operations of directors and actors
+    neo4j(args.user,args.password,args.hostname,text_lines)
 if __name__ == '__main__':
     main()
