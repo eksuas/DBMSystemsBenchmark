b996a62e5245bfc38602db67633bc239196279a4
IremmB <37192705+IremmB@users.noreply.github.com>
2018-07-10 12:18:07 +0300

    Create data set and read files
    Data set is created.After that,  FILMS.txt and Collectors.txt are readed.

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def read_files(args):
     try:
         # Open file with args.path
         films_file=open(args.films_path,"r")
-        # collectors_file=open(args.collectors_path,"r")
+        collectors_file=open(args.collectors_path,"r")
     except IOError:
          #if there is a error, do it
         print("The files are not found.")
@@ -46,12 +46,25 @@ def read_files(args):
     # Create data instance
     data = dataset.Data()
 
-    # Read Films file
-    lines=films_file.readlines()
+    # Read files
+    films_lines=films_file.readlines()
+    collectors_lines=collectors_file.readlines()
+    collector_id=1000
+    director_id=3000
+    actor_id=2000
+    actor_set=Set()
+    director_set=Set()
+    # Firstly, read informations in FILMS file
+    for i in xrange(len(films_lines)):
 
-    for i in xrange(len(lines)):
-        line=lines[i].split(' % ')
-        # Create dictionary for movies
+        line=films_lines[i].split(' % ')
+        actor_list=(line[3].split(', '))
+        #Add directors of movie into director set
+        director_set.add(line[5])
+        #Add actors of the movie into set
+        for actor in actor_list:
+            actor_set.add(actor)
+        #Send movie informations to dataset and create a movie
         movie = dataset.Movie(
             ID=line[0],
             title=line[1],
@@ -60,9 +73,43 @@ def read_files(args):
             director=line[5],
             rating=line[6]
         )
-        # Divide actors(line[3]=actors) incording to "," and add into actor_list
-        movie.actors = Set(line[3].split(', '))
+        #Add movie and into movies set in dataset
         data.movies.add(movie)
+        #Send informations of every director to Director class in dataset
+    for director in director_set:
+        #Create director
+        director=dataset.Director(
+            ID=director_id,
+            name=director
+        )
+        director_id+=1
+        # Add director into directors set in dataset
+        data.directors.add(director)
+
+
+    #Send every actor to Actor class in dataset
+    for actor in actor_set:
+        #Create actor
+        actor=dataset.Actor(
+            ID=actor_id,
+            name=actor
+
+        )
+        actor_id+=1
+
+        # Add actor into actors set in datasetc
+        data.actors.add(actor)
+
+    #Read lines in collectors file
+    for i in xrange(len(collectors_lines)):
+        line=collectors_lines[i].split('%')
+        #Send every collector to Collector class in dataset
+        collector = dataset.Collector(
+            ID=line[0],
+            name=line[1],
+        )
+        #Add collector into collectors set in data class
+        data.collectors.add(collector)
 
     return data
 
@@ -70,8 +117,7 @@ def read_files(args):
 def main():
     args=arg_parser()
     data=read_files(args)
-    for m in data.movies:
-        print m.title
+
 
 if __name__ == '__main__':
     main()
