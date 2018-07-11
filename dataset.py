59108754fe6580f83a1fe80654a5703634a1dec3
Edanur Demir <eksuas@gmail.com>
2018-07-11 19:39:12 +0300

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -13,7 +13,7 @@ class Movie:
         self.title = title
         self.year = year
         self.genre = genre
-        self.directors = director
+        self.director = director
         self.rating = rating
         self.actors = Set()
         self.collectors = Set()
